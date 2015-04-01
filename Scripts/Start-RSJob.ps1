9f165ec0a17f44be9e6f9a179f14a7c28cf28bfd
Boe Prox <boeprox@gmail.com>
2015-04-01 08:48:08 -0500

    Update Start-RSJob.ps1

--- Scripts/Start-RSJob.ps1
+++ Scripts/Start-RSJob.ps1
@@ -129,7 +129,7 @@ Function Start-RSJob {
     #>
     [OutputType('PoshRS.PowerShell.RSJob')]
     [cmdletbinding(
-        DefaultParameterSetName = 'ScriptBlock'
+        DefaultParameterSetName = 'All'
     )]
     Param (
         [parameter(Position=0,ParameterSetName = 'ScriptBlock')]
@@ -215,9 +215,7 @@ Function Start-RSJob {
             }
             Default {
                 Write-Debug "Using AST with PowerShell V3+"
-                $UsingVariables = GetUsingVariables $ScriptBlock | Group SubExpression | ForEach {
-                    $_.Group | Select -First 1
-                }
+                $UsingVariables = GetUsingVariables $ScriptBlock
                 If ($UsingVariables) {                    
                     $UsingVariableValues = @(GetUsingVariableValues $UsingVariables)
                     Write-Verbose "Found $($UsingVariableValues.Count) Using values"
