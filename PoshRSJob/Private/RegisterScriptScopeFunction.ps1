1b51f7010e6e05acce1f6b881b8939f489be5911
Cody Konior <cody@codykonior.com>
2017-02-18 14:27:14 +0800

    #126 further fix to allow nested functions
    After my previous fix I came up with one more test which failed - where
    &Test3.ps1 -> Func A { Start-RsJob }; Func B { A } B; would fail. This
    requires some looping to resolve.

--- PoshRSJob/Private/RegisterScriptScopeFunction.ps1
+++ PoshRSJob/Private/RegisterScriptScopeFunction.ps1
@@ -4,35 +4,47 @@
         [parameter()]
         [string[]]$Name
     )    
+    $FoundName = @()
     Write-Verbose "Getting callstacks"
     $PSCallStack = Get-PSCallStack
     Write-Verbose "PSCallStacks: `n$($PSCallStack|Out-String)"
     If ($PSCallStack.count -gt 1) {
-        #Ensure that I always get the second item in the call stack
-        $CallStack = $PSCallStack | Select-Object -First 1 -Skip 2
-        Switch ($PSVersionTable.PSVersion.Major) {
-            '2' {
-                $ScriptBlock = Get-Content $CallStack.ScriptName
-                $Functions = @(FindFunction -ScriptBlock ($ScriptBlock | Out-String))
-            }
-            Default {
-                $Flags = [System.Reflection.BindingFlags]'nonpublic,instance,static'
-                $FunctionContext = $CallStack.GetType().GetProperty('FunctionContext',$Flags).GetValue($CallStack,$Null)
-                $ScriptBlock = $FunctionContext.GetType().GetField('_scriptBlock',$Flags).GetValue($FunctionContext)
-                $Functions = @(($ScriptBlock.ast.FindAll({$args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst]},$False)))
-            }
-        }
-        Write-Verbose ("Found {0} functions" -f $Functions.count)
-        Write-Verbose "Functions found: `n$($Functions | Select -Expand Name|Out-String)"
-        $Functions | ForEach {
-            If ($PSBoundParameters.ContainsKey('Name')) {
-                If ($Name -contains $_.Name ) {
-                    Write-Verbose "Loading $($_.Name)" 
-                    .([scriptblock]::Create("Function Script:$($_.Name) $($_.Body)"))
+        foreach ($CallStack in ($PSCallStack | Select-Object -Skip 2)) {
+            Switch ($PSVersionTable.PSVersion.Major) {
+                '2' {
+                    $ScriptBlock = Get-Content $CallStack.ScriptName
+                    $Functions = @(FindFunction -ScriptBlock ($ScriptBlock | Out-String))
                 }
-            } Else {
-                Write-Verbose "Loading $($_.Name)" 
-                .([scriptblock]::Create("Function Script:$($_.Name) $($_.Body)"))
+                Default {
+                    $Flags = [System.Reflection.BindingFlags]'nonpublic,instance,static'
+                    $FunctionContext = $CallStack.GetType().GetProperty('FunctionContext',$Flags).GetValue($CallStack,$Null)
+                    $ScriptBlock = $FunctionContext.GetType().GetField('_scriptBlock',$Flags).GetValue($FunctionContext)
+                    $Functions = @(($ScriptBlock.ast.FindAll({$args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst]},$False)))
+                }
+            }
+            Write-Verbose ("Found {0} functions" -f $Functions.count)
+            Write-Verbose "Functions found in callstack $Callstack`n$($Functions | Select-Object -Expand Name|Out-String)"
+            if ($Functions) {
+                $Functions | ForEach-Object {
+                    If ($PSBoundParameters.ContainsKey('Name')) {
+                        If ($Name -contains $_.Name -and $FoundName -notcontains $_.Name) {
+                            Write-Verbose "Loading $($_.Name)" 
+                            $FoundName += $_.Name
+                            .([scriptblock]::Create("Function Script:$($_.Name) $($_.Body)"))
+                        }
+                    } Else {
+                        if ($FoundName -notcontains $_.Name) {
+                            Write-Verbose "Loading $($_.Name)" 
+                            $FoundName += $_.Name
+                            .([scriptblock]::Create("Function Script:$($_.Name) $($_.Body)"))
+                        }
+                    }
+                }
+
+                # Stop searching callstacks once we found what we want   
+                if ($Name.Count -eq $FoundName.Count) {       
+                    break
+                }
             }
         }
     }
